054be41be0309d0f731b4cbccc4c345323aecf6a
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-06-18 15:57:19 +0530

    Bolt List Pojo

---
+++ app/src/main/java/com/example/site_supervisor/BoltListActivity.java
@@ -0,0 +1,20 @@
+package com.example.site_supervisor;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class BoltListActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bolt_list);
+
+    }
+}
