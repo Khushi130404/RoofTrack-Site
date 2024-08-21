c3176e877cf93461287a80ba656e0c76ccabc480
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-21 17:33:25 +0530

    DeliveryChallanPojo.java

---
+++ app/src/main/java/com/example/site_supervisor/DeliveryChallanPojo.java
@@ -0,0 +1,49 @@
+package com.example.site_supervisor;
+
+public class DeliveryChallanPojo {
+    int id;
+    String code;
+    String itemName;
+    float qty;
+    String unit;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public float getQty() {
+        return qty;
+    }
+
+    public void setQty(float qty) {
+        this.qty = qty;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+}
