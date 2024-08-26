26c84dd388a08076321035a7e14221cf20b68a53
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-26 12:48:01 +0530

    StockMaterialActivity findViewById

--- app/src/main/java/com/example/site_supervisor/StockMaterialActivity.java
+++ app/src/main/java/com/example/site_supervisor/StockMaterialActivity.java
@@ -1,7 +1,13 @@
 package com.example.site_supervisor;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +15,34 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 public class StockMaterialActivity extends Activity {
+
+    TextView tvDate;
+    List<StockMaterialPojo> stock;
+    SQLiteDatabase db = null;
+    public String dbPath = "/data/data/com.example.site_supervisor/databases/";
+    public static String dbName= "Site_Supervisor.db";
+    String path = dbPath+dbName;
+    ListView listStock;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_material);
+
+        tvDate = findViewById(R.id.tvDate);
+        listStock = findViewById(R.id.listStock);
+
+        try
+        {
+            db = SQLiteDatabase.openDatabase(path,null,SQLiteDatabase.OPEN_READWRITE);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(getApplicationContext(),"Error : "+e.getMessage(),Toast.LENGTH_LONG).show();
+        }
     }
 }
