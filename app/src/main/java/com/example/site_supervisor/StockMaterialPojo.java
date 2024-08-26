ec5cf2c6f9c1ae533ce4f7fb6dedcd5428402d17
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-26 12:18:31 +0530

    stock_material_adapter.xml

--- app/src/main/java/com/example/site_supervisor/StockMaterialPojo.java
+++ app/src/main/java/com/example/site_supervisor/StockMaterialPojo.java
@@ -12,47 +12,36 @@ public class StockMaterialPojo
     public int getPos() {
         return pos;
     }
-
     public void setPos(int pos) {
         this.pos = pos;
     }
-
     public String getCode() {
         return code;
     }
-
     public void setCode(String code) {
         this.code = code;
     }
-
     public String getItemname() {
         return itemname;
     }
-
     public void setItemname(String itemname) {
         this.itemname = itemname;
     }
-
     public float getDcQty() {
         return dcQty;
     }
-
     public void setDcQty(float dcQty) {
         this.dcQty = dcQty;
     }
-
     public float getUsedQty() {
         return usedQty;
     }
-
     public void setUsedQty(float usedQty) {
         this.usedQty = usedQty;
     }
-
     public float getStockQty() {
         return stockQty;
     }
-
     public void setStockQty(float stockQty) {
         this.stockQty = stockQty;
     }
