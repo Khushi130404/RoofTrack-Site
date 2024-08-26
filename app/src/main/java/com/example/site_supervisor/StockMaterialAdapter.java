dac6b99b97b4341f0b7f31dd0ce80bf88647d90c
Patel Khushi Sanjivbhai <155909246+Khushi130404@users.noreply.github.com>
2024-08-26 12:35:52 +0530

    StockMaterialAdapter.java

--- app/src/main/java/com/example/site_supervisor/StockMaterialAdapter.java
+++ app/src/main/java/com/example/site_supervisor/StockMaterialAdapter.java
@@ -2,7 +2,11 @@ package com.example.site_supervisor;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 
@@ -23,5 +27,27 @@ public class StockMaterialAdapter extends ArrayAdapter
         this.cont = cont;
         this.resource = resource;
         this.stock = stock;
+    }
+
+    public View getView(final int position, View convetView, ViewGroup parent)
+    {
+        LayoutInflater inflater = LayoutInflater.from(cont);
+        View view = inflater.inflate(resource,null,false);
+        int id[] = {R.id.etPosition,R.id.etCode,R.id.etItemName,R.id.etDCQty,R.id.etUsedQty,R.id.etStockQty};
+        EditText et[] = new EditText[6];
+
+        for(int i=0; i<id.length; i++)
+        {
+            et[i] = view.findViewById(id[i]);
+        }
+
+        et[0].setText(""+position+1);
+        et[1].setText(stock.get(position).getCode());
+        et[2].setText(stock.get(position).getItemname());
+        et[3].setText(""+stock.get(position).getDcQty());
+        et[4].setText(""+stock.get(position).getUsedQty());
+        et[5].setText(""+stock.get(position).getStockQty());
+
+        return view;
     }
 }
